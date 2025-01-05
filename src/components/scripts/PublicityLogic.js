feee16ff458b6b0b45b8154108210f6e8f347f74
ahnochen1029 <ahnochen1029@gmail.com>
2025-01-05 23:27:53 +0800

    Update deployment

--- src/components/scripts/PublicityLogic.js
+++ src/components/scripts/PublicityLogic.js
@@ -19,7 +19,7 @@ export default {
                 if (result[item] && config[item] > 0) {
                     message.push({
                         key: index + 1,
-                        title: `${label}抽獎结果:`,
+                        title: `${label}抽獎結果:`,
                         value: `${result[item].length > 0 ? result[item].join('、') : '暫未抽出'
                             }`
                     });
